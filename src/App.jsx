818951e37233f103a85e0a9a3a76db13d58b0aea
premchandellendula <premcellendula@gmail.com>
2024-10-01 10:38:27 +0530

    footer update

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   return (
     <div>
       <Navbar/>
-      <h2>This is a sample react file</h2>
-      <p>This project is built to learn about the branches and how to use different branches for different features</p>
+
+      <div className="h-screen">
+        <h2>This is a sample react file</h2>
+        <p>This project is built to learn about the branches and how to use different branches for different features</p>
+      </div>
 
       <Footer />
     </div>
