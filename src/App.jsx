9af02df1561ea3e8a157fcaf771f8dafed96f3fd
premchandellendula <premcellendula@gmail.com>
2024-10-01 08:48:33 +0530

    head and para

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div>
+      <h2>This is a sample react file</h2>
+      <p>This project is built to learn about the branches and how to use different branches for different features</p>
+    </div>
+  )
+}
+
+export default App
